ad7d7a402cbe947d27558afe9de8414cd0506e51
Viktor Mahler <vikitorony@gmail.com>
2020-04-21 17:06:42 +0200

    Users + Groups CRUD

--- migrations/20200421132519_create-groups.js
+++ migrations/20200421132519_create-groups.js
@@ -1,16 +1,16 @@
 
 exports.up = (knex) => {
-  return knex.schema.createTable('users', (table) => {
+  return knex.schema.createTable('groups', (table) => {
     table.increments();
-    table.string('firstName');
-    table.string('lastName');
-    table.string('email');
-    table.integer('age');
+    table.string('name');
+    table.string('description');
+    table.string('location');
+    table.integer('maximalSize');
     table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP')); // table.timestamps(true, true);
     table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
   });
 };
 
 exports.down = (knex) => {
-  return knex.schema.dropTable('users');
+  return knex.schema.dropTable('groups');
 };
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const app: Application = express();
 app.use(express.json());
 const { PORT = 3000 } = process.env;
 
+
+// USER
 interface User {
   id?: number;
   firstName: string;
@@ -61,10 +63,10 @@ app.post('/user', async (req: Request, res: Response) => {
 });
 
 // update
-app.put('/user:id', async (req: Request, res: Response) => {
+app.put('/user/:id', async (req: Request, res: Response) => {
   try {
     const user: User = await database('users').select().where({ id: req.params.id }).first();
-    if (user) {
+    if (typeof user !== 'undefined') { // (user) vagy (typeof user !== 'undefined')
       const newUser: User = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -85,14 +87,100 @@ app.put('/user:id', async (req: Request, res: Response) => {
 // delete
 app.delete('/user/:id', async (req: Request, res: Response) => {
   try {
-    const user: User = await database('users').select().where({id: req.params.id}).first();
+    const user: User = await database('users').select().where({ id: req.params.id }).first();
     if (user) {
-      await database('users').delete().where({id:req.params.id});
+      await database('users').delete().where({ id: req.params.id });
       res.sendStatus(204);
     } else {
       res.sendStatus(404);
     }
-  } catch(error) {
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// GROUP
+
+interface Group {
+  id?: string;
+  name: string;
+  description: string;
+  location: string;
+  maximalSize: number;
+}
+
+// index
+app.get('/group', async (req: Request, res: Response) => {
+  const groups: Array<Group> = await database('groups').select();
+  res.json(groups);
+})
+
+// show
+app.get('/group/:id', async (req: Request, res: Response) => {
+  try {
+    const group: Group = await database('groups').select().where({ id: req.params.id }).first();
+    if (group) {
+      res.json(group);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// create
+app.post('/group', async (req: Request, res: Response) => {
+  try {
+    const group: Group = {
+      name: req.body.name,
+      description: req.body.description,
+      location: req.body.location,
+      maximalSize: req.body.maximalSize // vagy parseInt(req.body.maximalSize)
+    }
+    await database('groups').insert(group);
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// update
+app.put('/group/:id', async (req: Request, res: Response) => {
+  try {
+    const group: Group = await database('groups').select().where({ id: req.params.id }).first();
+    if (typeof group !== 'undefined') { // (group) vagy (typeof group !== 'undefined')
+      const newGroup: Group = {
+        name: req.body.name,
+        description: req.body.description,
+        location: req.body.location,
+        maximalSize: req.body.maximalSize
+      }
+      await database('groups').update(newGroup).where({ id: req.params.id });
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// delete
+app.delete('/group/:id', async (req: Request, res: Response) => {
+  try {
+    const group: Group = await database('groups').select().where({ id: req.params.id }).first();
+    if (group) {
+      await database('groups').delete().where({ id: req.params.id });
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
     console.error(error);
     res.sendStatus(500);
   }
