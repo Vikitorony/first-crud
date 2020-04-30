068b16b2076d40ae7f9ed64ec2c942c5fa8643de
Viktor Mahler <vikitorony@gmail.com>
2020-04-30 14:48:57 +0200

    swagger /login

--- src/app/routers/group.ts
+++ src/app/routers/group.ts
@@ -3,9 +3,9 @@ import * as groupController from '../controllers/group';
 
 export const router: Router = Router({ mergeParams: true });
 
-router.use(groupController.authorization);
 router.get('/', groupController.index);           // index
 router.get('/:id', groupController.show);        // show
+router.use(groupController.authorization);
 router.post('/', groupController.create);         // create
 router.put('/:id', groupController.update);      // update
 router.delete('/:id', groupController.destroy);  // delete
